561af86e014892478d00c19baae320525bae5420
damlacim <cim.damla@gmail.com>
2022-02-28 02:41:50 +0300

    Initial Commit

---
+++ Bill Splitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill Splitter
+//
+//  Created by Damla Çim on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
