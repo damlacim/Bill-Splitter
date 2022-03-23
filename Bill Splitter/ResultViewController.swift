8ae016bcdab2ce9ed232556a061656d7446286dd
damlacim <cim.damla@gmail.com>
2022-03-23 13:21:48 +0300

    configure stepper and create alert

--- Bill Splitter/ResultViewController.swift
+++ Bill Splitter/ResultViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +21,8 @@ class ResultViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: Any) {
     }
-    */
+    
 
 }
